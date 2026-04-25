2babe3192f54b437f61cf76285dcd17f6b96a195
Achal Gupta <53587064+gupta-achal@users.noreply.github.com>
2026-04-25 13:44:53 +0530

    Time: 28 ms (9.69%) | Memory: 47.9 MB (43.98%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int li = 1;
+        int hi = Arrays.stream(piles).max().getAsInt();
+
+        while(li <= hi){
+            int mid = li + (hi-li)/2;
+            int tr = timeRequiredToEatKbananas(piles,mid);
+
+            if(tr <= h){
+                hi = mid-1;
+            }else{
+                li = mid+1;
+            }
+        }
+
+
+
+        return li;
+    }
+    private static int timeRequiredToEatKbananas(int[] piles, int k){
+        int h = 0;
+
+        for(int b: piles){
+            h += Math.ceil((double)b/(double)k);
+        }
+        return h;
+    }
+}
