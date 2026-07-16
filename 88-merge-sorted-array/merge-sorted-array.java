765f7f40f1da9c726df6192ac91eb40958c71a75
Achal Gupta <53587064+gupta-achal@users.noreply.github.com>
2026-07-16 21:37:59 +0530

    Time: 16 ms (4.15%) | Memory: 44.2 MB (8.91%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -2,19 +2,19 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         reverse(nums1,0,m+n-1);
         reverse(nums1,n,m+n-1);
+
         for(int x: nums1){
             System.out.print(x+" ");
         }
-        int i = n; 
-        int j = 0;
-        int l1 = nums1.length-1, l2=nums2.length-1;
+
+        int i = n;
+        int j=0;
         int k=0;
-        while(i<=l1 && j<=l2){
+        while(i < m+n && j<n){
             if(nums1[i] <= nums2[j]){
                 nums1[k] = nums1[i];
-                
-                k++;
                 i++;
+                k++;
             }else{
                 nums1[k] = nums2[j];
                 k++;
@@ -22,20 +22,18 @@ class Solution {
             }
         }
 
-        while(j<=l2){
+        while(j<n){
             nums1[k] = nums2[j];
             k++;
             j++;
         }
 
-        
     }
-
-    private static void reverse(int [] arr, int l, int r){
+    private static void reverse(int[] num, int l, int r){
         while(l<=r){
-            int temp = arr[l];
-            arr[l] = arr[r];
-            arr[r] = temp;
+            int t = num[l];
+            num[l] = num[r];
+            num[r] = t;
             l++;
             r--;
         }
