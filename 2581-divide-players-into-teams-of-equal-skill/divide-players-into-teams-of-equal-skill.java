22944f3d8d57701f552f9890ba045ef08794be8f
Achal Gupta <53587064+gupta-achal@users.noreply.github.com>
2026-07-27 23:09:29 +0530

    Time: 36 ms (5.13%) | Memory: 74.7 MB (92.16%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,19 +1,40 @@
 class Solution {
-    public long dividePlayers(int[] arr) {
-        Arrays.sort(arr);
-        int l=0, r=arr.length-1;
-        int sum = arr[l] + arr[r];
-        long ans = 0;
-        while(l<r){
-            int s = arr[l] + arr[r];
-            if(s!=sum){
-                return -1;
-            }
+    public long dividePlayers(int[] skill) {
 
-            ans += arr[l] * arr[r];
-            l++;
-            r--;
+        int n = skill.length;
+        int sum = Arrays.stream(skill).sum();
+
+        if (sum % (n / 2) != 0) {
+            return -1;
         }
+
+        int target = sum / (n / 2);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int x : skill) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        long ans = 0;
+
+        for (int x : skill) {
+
+            if (map.get(x) == 0)
+                continue;
+
+            map.put(x, map.get(x) - 1);
+
+            int need = target - x;
+
+            if (map.getOrDefault(need, 0) == 0)
+                return -1;
+
+            map.put(need, map.get(need) - 1);
+
+            ans += (long) x * need;
+        }
+
         return ans;
     }
 }
